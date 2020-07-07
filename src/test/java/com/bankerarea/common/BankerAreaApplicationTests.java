ae05b6adf63f58f1084b6302a3e5dea771a6bcc1
formegusto <formegusto@gmail.com>
2020-07-07 15:14:47 +0900

    Init

---
+++ src/test/java/com/bankerarea/common/BankerAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankerarea.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
